916cca90910ee15d8babf08afbdacd6b9cff56d8
zoa-sh <zoa@zoa.sh>
2024-09-16 19:58:15 +0000

    add new project

--- src/mods/projects.rs
+++ src/mods/projects.rs
@@ -9,6 +9,7 @@ pub struct Project {
     pub description: String,
     pub tech: Vec<String>,
     pub repo: String,
+    pub org: String,
 }
 
 // modify this to pull from git
@@ -16,7 +17,7 @@ pub fn get_projects() -> Vec<Project> {
     vec![
         Project {
             name: "This site!".to_string(),
-            description: "The site your looking at right now!".to_string(),
+            description: "The site you're looking at right now!".to_string(),
             tech: vec![
                 "Rust".to_string(),
                 "Rocket".to_string(),
@@ -24,29 +25,24 @@ pub fn get_projects() -> Vec<Project> {
                 "CSS".to_string(),
                 "HTML".to_string(),
             ],
-            repo: "zoa-sh".to_string(),
+            repo: "zoa.sh".to_string(),
+            org: "zoa-sh".to_string(),
         },
         Project {
-            name: "Project 2".to_string(),
-            description: "Description of Project 2".to_string(),
-            tech: vec![
-                "Python".to_string(),
-                "Django".to_string(),
-                "PostgreSQL".to_string(),
+            name: "NyxPSI".to_string(),
+            description: "Next-gen network protocol for reliable data transfer in lossy environments. Outperforms TCP/UDP in high packet loss scenarios.".to_string(),
+            tech: vec!["Rust".to_string(),
+                "UDP".to_string(),
+                "FEC".to_string(),
             ],
-            repo: "project-2".to_string(),
-        },
-        Project {
-            name: "Project 3".to_string(),
-            description: "Description of Project 3".to_string(),
-            tech: vec!["Python".to_string(), "CSS".to_string()],
-            repo: "project-3".to_string(),
+            repo: "nyxpsi".to_string(),
+            org: "nyxpsi".to_string(),
         },
     ]
 }
 
-#[get("/redirect/<repo>")]
-pub fn redirect(repo: &str) -> Redirect {
-    let base_url = "https://git.zoa.sh/";
-    Redirect::to(format!("{}{}.git", base_url, repo))
+#[get("/redirect/<org>/<repo>")]
+pub fn redirect(org: &str, repo: &str) -> Redirect {
+    let base_url = "https://github.com/";
+    Redirect::to(format!("{}{}/{}", base_url, org, repo))
 }
